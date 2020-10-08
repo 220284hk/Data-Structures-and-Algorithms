d0cf117f5eb735836f419416e98a471bbac0e1f2
hyun <220284hk@gmail.com>
2020-10-09 00:02:07 +0900

    Princeton JAR cannot be found

--- Algorithms part 1/Week 2/myImplementationsW2/Permutation.java
+++ Algorithms part 1/Week 2/myImplementationsW2/Permutation.java
@@ -1,20 +1,20 @@
-package myImplementationsW2;
-
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
-import queues.RandomizedQueue;
-
-public class Permutation {
-    public static void main(String[] args) {
-        RandomizedQueue<String> rq = new RandomizedQueue<>();
-        int k = Integer.parseInt(args[0]);
-        while (!StdIn.isEmpty()) {
-            rq.enqueue(StdIn.readString());
-        }
-
-        while (k > 0) {
-            StdOut.println(rq.dequeue());
-            k--;
-        }
-    }
-}
+//package myImplementationsW2;
+//
+//import edu.princeton.cs.algs4.StdIn;
+//import edu.princeton.cs.algs4.StdOut;
+//import queues.RandomizedQueue;
+//
+//public class Permutation {
+//    public static void main(String[] args) {
+//        RandomizedQueue<String> rq = new RandomizedQueue<>();
+//        int k = Integer.parseInt(args[0]);
+//        while (!StdIn.isEmpty()) {
+//            rq.enqueue(StdIn.readString());
+//        }
+//
+//        while (k > 0) {
+//            StdOut.println(rq.dequeue());
+//            k--;
+//        }
+//    }
+//}
